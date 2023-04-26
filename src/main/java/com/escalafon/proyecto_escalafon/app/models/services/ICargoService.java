815d2fbe5ec26fdba2b8972fea2164da0fa96287
Andy Fernandez Erazo <andy.erazofernandez@gmail.com>
2023-04-26 16:16:58 -0500

    Prueba

--- src/main/java/com/escalafon/proyecto_escalafon/app/models/services/ICargoService.java
+++ src/main/java/com/escalafon/proyecto_escalafon/app/models/services/ICargoService.java
@@ -14,6 +14,7 @@ public interface ICargoService {
     public CargoRes finOne(@NonNull  Integer id);
     public CargoRes updateCargo(@NonNull Integer id, @NonNull CargoRes cargoRes);
     public void delete(@NonNull Integer id);
+    public void prueba(@NonNull Integer id);
 
 
 }
