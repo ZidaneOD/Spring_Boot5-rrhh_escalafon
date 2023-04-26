c4f2b620e96e90265d8136479204e18998dd11c4
Zidane Ortiz <zidaneortiz16@gmail.com>
2023-04-26 16:47:47 -0500

    modify: Reestructura del ProyectoEscalafonApplication - Login

--- src/main/java/com/escalafon/proyecto_escalafon/app/controllers/UsuarioController.java
+++ src/main/java/com/escalafon/proyecto_escalafon/app/controllers/UsuarioController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RequestMapping(path = "api/usuario")
 public class UsuarioController {
     @Autowired
+    @Qualifier(value = "IUsuarioServiceImp")
     private IUsuarioService usuarioService;
 
     @GetMapping
