2b6c8727b7f5cc70e8ddf7b4cfb4f6bd63f907e8
Andy Fernandez Erazo <andy.erazofernandez@gmail.com>
2023-05-05 15:54:51 -0500

    Avance parcial
    rrhh_escalafon\src\main\java\com\escalafon\proyecto_escalafon\app\infraestructures\dao\IRegimenPensionDao.java

---
+++ src/main/java/com/escalafon/proyecto_escalafon/app/models/resquests/RegimenPensionResquest.java
@@ -0,0 +1,18 @@
+package com.escalafon.proyecto_escalafon.app.models.resquests;
+
+import com.escalafon.proyecto_escalafon.app.models.entitys.AfpEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class RegimenPensionResquest {
+    @JsonProperty("idRegimen")
+    private Integer idRegimen;
+    @JsonProperty("nombreRegimen")
+    private String nombreRegimen;
+    @JsonProperty("afp")
+    private AfpEntity afp;
+}
